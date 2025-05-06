aa49c6f7173c7b0390a25a075a63fdcfc9cc213c
Mushfique Alam <162487127+MushfiqueAlam@users.noreply.github.com>
2025-05-06 19:44:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const timer=document.querySelector("#timer");
+setInterval(()=>{
+	let date=new Date();
+	timer.innerHTML=`${date.toLocaleDateString()}, ${date.toLocaleTimeString()}`;
+},1000)
